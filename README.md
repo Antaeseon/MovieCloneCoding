1a14cf9d3d98cc6e96f01d74882a6e635b320510
taeseon An <taeseonan@taeseonui-MacBookPro.local>
2020-05-31 15:43:42 +0900

    chap 4 clear

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MovieCloneCoding
+# Movie Clone Coding
+
+리액트 JS를 이용한 2020 클론코딩 프로젝트
--- ts_movie_app/src/App.js
+++ ts_movie_app/src/App.js
@@ -1,22 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 
-function Food({fav}){
-  return <h1>I like {fav}</h1>
+function Food({name, picture, rating}){
+  return(
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src = {picture} alt = {name} />
+    </div>
+  )
 }
+
+
+const foodIlike = [
+  {
+    id: 1,
+    name : 'Kimchi',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JNs3joXdaQnOz5npvjBCBwHaEK%26pid%3DApi&f=1',
+    rating : 5,
+  },
+  {
+    id : 2,
+    name : 'Samgyeopsal',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Mz1Q9GnOjEBnQgBQ4c6plwHaEK%26pid%3DApi&f=1',
+    rating : 4.9,
+  },
+  {
+    id : 3,
+    name : 'bibimabap',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.sg3EilfZgNZTyq-vRRfzGAHaEK%26pid%3DApi&f=1',
+    rating : 4.8,
+  }
+];
+
 
 function App() {
   return(
     <div>
-      <h1>
-        Hello
-      </h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodIlike.map(dish=>(
+        <Food key= {dish.id} name={dish.name} picture={dish.image} rating = {dish.rating}/>
+      ))}
     </div> 
   )
 }
+
+Food.propTypes={
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number,
+};
+
 
 export default App;
