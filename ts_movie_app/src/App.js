bfb8db2d5cc998c1a042ab989dc3b1e58d524672
taeseon An <taeseonan@taeseonui-MacBookPro.local>
2020-06-01 00:29:59 +0900

    chap 6 clear

--- ts_movie_app/src/App.js
+++ ts_movie_app/src/App.js
@@ -1,55 +1,57 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css'
 
+class App extends React.Component{
 
-function Food({name, picture, rating}){
-  return(
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src = {picture} alt = {name} />
-    </div>
-  )
-}
+  state = {
+    isLoading : true,
+    movies : [],
+  };
 
+  getMovies=async ()=>{
+    const {
+      data : {
+        data : {movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
 
-const foodIlike = [
-  {
-    id: 1,
-    name : 'Kimchi',
-    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JNs3joXdaQnOz5npvjBCBwHaEK%26pid%3DApi&f=1',
-    rating : 5,
-  },
-  {
-    id : 2,
-    name : 'Samgyeopsal',
-    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Mz1Q9GnOjEBnQgBQ4c6plwHaEK%26pid%3DApi&f=1',
-    rating : 4.9,
-  },
-  {
-    id : 3,
-    name : 'bibimabap',
-    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.sg3EilfZgNZTyq-vRRfzGAHaEK%26pid%3DApi&f=1',
-    rating : 4.8,
+    this.setState({movies:movies, isLoading : false})
+    
   }
-];
 
 
-function App() {
-  return(
-    <div>
-      {foodIlike.map(dish=>(
-        <Food key= {dish.id} name={dish.name} picture={dish.image} rating = {dish.rating}/>
-      ))}
-    </div> 
-  )
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading 
+        ? (
+          <div class = "loader">
+            <span class = "loader__text">Loading</span>
+          </div>
+        ) 
+          : (
+            <div class = "movies">
+              {movies.map((movie) => (
+                 <Movie 
+                  id = {movie.id}
+                  year = {movie.year}
+                  title = {movie.title}
+                  summary = {movie.summary}
+                  poster = {movie.medium_cover_image}
+                />
+                ))}
+            </div>
+      )}
+      </section>
+      );
+  }
 }
-
-Food.propTypes={
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number,
-};
-
 
 export default App;
